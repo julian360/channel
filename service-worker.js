1a3231242e71c29efbd5c9b35c4d272f5942122f
julian360 <julian_lusuardi@hotmail.com>
2025-07-21 19:37:35 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'channel-app-v1.7.5'; // <--- ¡IMPORTANTE! Cambia esto con cada nueva implementación principal
+const CACHE_NAME = 'channel-app-v1.7.9'; // <--- ¡IMPORTANTE! Cambia esto con cada nueva implementación principal
 const urlsToCache = [
     '/channel/index.html', // Ruta actualizada
     '/channel/manifest.json', // Ruta actualizada
